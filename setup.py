bbbdbd4c1b83f6296b792bb3f0da7d437e5bdc71
jcarmac3 <pokemonpower92@gmail.com>
2023-05-12 09:35:42 -0600

    update .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='data_ingester',
+      version='1.0',
+      description='Data ingester for accountability.',
+      author='Pokemonpower',
+      packages=['app'],
+     )
