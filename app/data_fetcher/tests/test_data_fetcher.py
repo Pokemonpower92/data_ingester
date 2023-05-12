f5ebfa0b201a731faac4fef2712d758c976d756e
jcarmac3 <pokemonpower92@gmail.com>
2023-05-12 12:46:20 -0600

    Unit tests doneish

---
+++ app/data_fetcher/tests/test_data_fetcher.py
@@ -0,0 +1,29 @@
+from unittest import TestCase, mock
+from requests import Response
+
+from app.data_fetcher.datafetcher import DataFetcher
+
+mock_request = {
+    "endpoint": "blah",
+    "headers": {"fake": "fake"}
+}
+
+
+def mock_get(endpoint, headers):
+    mock_response = Response()
+    mock_response.url = endpoint
+    mock_response.headers = headers
+    return mock_response
+
+
+class TestDataFetcher(TestCase):
+
+    @mock.patch("app.data_fetcher.datafetcher.requests.get")
+    @mock.patch("app.data_fetcher.datafetcher.requests.Response.json")
+    def test_fetch(self, mocked_json, mocked_get):
+        data_fetcher = DataFetcher()
+        mocked_get.return_value = mock_get("blah", {"fake": "fake"})
+        mocked_json.return_value = mock_request
+        result = data_fetcher.fetch(mock_request["endpoint"], mock_request["headers"])
+
+        assert result == mock_request
